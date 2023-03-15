852cba2eba1665658cddfec222b107644e72e004
Cassidy <cassidy.vance@hotmail.com>
2023-03-15 15:49:34 -0700

    movie list

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let message = document.querySelector('#message')
+
+
+
+const addMovie = (evt) => {
+    evt.preventDefault()
+
+    let inputField = document.querySelector('input')
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ('')
+    console.log(evt)
+}
+
+let form = document.querySelector('form')
+
+form.addEventListener('submit', addMovie)
+
+
+const deleteMovie = (evt) => {
+    evt.target.parentNode.remove()
+    message.textContent = '0/10 would not recommend'
+}
+
+
+const crossOffMovie = (evt) => {
+    evt.target.classList.toggle('checked')
+    if(evt.target.classList.contains('checked') === true){
+        message.textContent = 'Get a hobby!'
+    } else {
+        message.textContent = 'Please find more friends before adding more movies'
+    }
+}
